b13c56681e4ebdb8d2a0299d1a3d6bda6a6d87ef
dorismireya <mir.nereidas@gmail.com>
2017-03-13 14:19:04 -0400

    Primer git vistas

---
+++ resources/views/principal/principalAdmin.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bienvenido</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="{!! asset('css/bootstrap.min.css') !!}">
+		<script src="{!! asset('js/bootstrap.min.js')!!}"></script>
+		<script src="{!! asset('js/jquery-3.1.1.min.js') !!}"></script>
+
+</head>
+<body>
+	<nav class="navbar navbar-inverse">
+	  <div class="container-fluid">
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>                        
+	      </button>
+	      <a class="navbar-brand" href="#">Logo</a>
+	    </div>
+	   
+	      <ul class="nav navbar-nav navbar-right">
+	        <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Salir</a></li>
+	      </ul>
+	    </div>
+	  </div>
+	</nav>
+	  
+	<div class="container-fluid text-center">    
+	  <div class="row content">
+	    <div class="col-sm-3 sidenav">
+	      <p><a href="#">Link</a></p>
+	      <p><a href="#">Link</a></p>
+	      <p><a href="#">Link</a></p>
+	    </div>
+	    <div class="col-sm-9 text-left"> 
+	      <h1>Bienvenido</h1>
+	      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+	      <hr>
+	      <h3>Test</h3>
+	      <p>Lorem ipsum...</p>
+	    </div>
+	  </div>
+	</div>
+
+	<footer class="container-fluid">
+	  <p>Developer for Mireya Terceros Ovando</p>
+	</footer>
+
+</body>
+</html>
---
+++ resources/views/usuarios/login.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login</title>
+	<link rel="stylesheet" type="text/css" href="{!! asset('css/bootstrap.min.css') !!}">
+    <link rel="stylesheet" type="text/css" href="{!! asset('css/bootstrap-theme.min.css') !!}">
+    <link rel="stylesheet" type="text/css" href="{!! asset('css/estilo.css') !!}">
+</head>
+<body>
+	<div class="container">
+	    <div class="row">
+	        <div class="col-sm-6 col-md-4 col-md-offset-4">
+	            <h1 class="text-center login-title">Ingrese sus Datos</h1>
+	            <div class="account-wall">
+	                <img class="profile-img" src="{!! asset('imagen/img_avatar2.png') !!}"
+	                    alt="">
+	                <form class="form-signin">
+	                <input type="text" class="form-control" placeholder="Nombre de Usuario" required autofocus>
+	                <input type="password" class="form-control" placeholder="Contraseña" required>
+	                <button class="btn btn-lg btn-primary btn-block" type="submit">
+	                    Ingresar</button>
+	                </form>
+	            </div>
+	            <a href="#" class="text-center new-account">Crear Cuenta</a>
+	        </div>
+	    </div>
+	</div>
+	<script src="{!! asset('js/jquery-3.1.1.min.js') !!}"></script>
+	<script src="{!! asset('js/bootstrap.min.js') !!}"></script>
+
+</body>
+</html>
